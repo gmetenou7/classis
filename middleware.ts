6df66b700d0ef0b8fe4244be3c679070d9a9414c
Bright Efoo <brightefoo@gmail.com>
2024-03-18 06:29:03 +0100

    fix type checking

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import acceptLanguage from 'accept-language'
 import { fallbackLng, languages } from '@/app/i18n/settings'
 
@@ -10,9 +10,9 @@ export const config = {
 
 const cookieName = 'i18next'
 
-export function middleware(req) {
+export function middleware(req : NextRequest) {
   let lng
-  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
+  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)?.value)
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
   if (!lng) lng = fallbackLng
 
@@ -21,7 +21,7 @@ export function middleware(req) {
   }
 
   if (req.headers.has('referer')) {
-    const refererUrl = new URL(req.headers.get('referer'))
+    const refererUrl = new URL(req.headers.get('referer') || "")
     const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`))
     const response = NextResponse.next()
     if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
