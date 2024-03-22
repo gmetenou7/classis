d652829aebdba0574a9e6b9c750c8273929690cc
Bright Efoo <brightefoo@gmail.com>
2024-03-22 10:26:02 +0100

    prise en charge de la hauteur des formulaires

--- src/app/[locale]/(authentification)/layout.tsx
+++ src/app/[locale]/(authentification)/layout.tsx
@@ -1,11 +1,14 @@
 import AuthSlider from "@/components/commons/AuthSlider";
+import styles from "@/styles/auth.module.css";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <section className="container mx-auto flex gap-8 p-4 items-stretch">
-        <aside className="w-full lg:w-1/2">{children}</aside>
+        <aside className={`w-full lg:w-1/2 ${styles["form-size"]}`}>
+          {children}
+        </aside>
         <AuthSlider classes="w-1/2 hidden lg:block" />
       </section>
     </div>
