853fb3e8e80fecfe629f7da751a1febdd0cd759b
Bright Efoo <brightefoo@gmail.com>
2024-03-22 10:05:26 +0100

    Correction des message d'erreur

--- src/app/[locale]/(authentification)/login/schema.ts
+++ src/app/[locale]/(authentification)/login/schema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 const schema = z.object({
-  email: z.string().email({ message: "Invalid email" }),
+  email: z.string().email({ message: "Email invalide" }),
   password: z.string().min(8, {
-    message: "Votre mot de passe doit password must have at least 8 characters !",
+    message: "Votre mot de passe doit doit avoir au moins 8 caractères !",
   }),
 });
 
